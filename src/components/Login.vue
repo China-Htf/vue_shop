41d0a1c3faeddcc9f05c491951c58ab05c429cf3
韩腾飞 <837183807@qq.com>
2021-03-26 21:36:40 +0800

    完成用户列表功能的开发

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -63,12 +63,13 @@ export default {
         if(!valid) return;
         // data是接口数据的一个属性，res被const声明（res可随意设置）
         const { data : res} = await this.$http.post("login", this.loginFrom);
+
         if(res.meta.status !== 200) return this.$message.error('登陆失败');
         this.$message.success('登陆成功');
         // 1 将登录成功之后的 token，保存到客户端的 sessionStorage 中
         //  1.1 项目中除了登陆之外的其它API接口，必须在登录之后才能访问
         //  1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
-        console.log(res);
+
         window.sessionStorage.setItem("token", res.data.token)
         // 2 通过编程式导航跳转到后台主页，路由地址是/home
         this.$router.push("/home")
